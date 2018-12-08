46b01927b09f10456830a965e593e8aa9568ceae
miaojinshuai <857560481@qq.com>
2018-12-08 20:33:55 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+with tf.device('/gpu:1'):
+    a = tf.constant([1.0, 2.0, 3.0], shape=[3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
+    c = a + b
+    # 通过log_device_placement参数来输出运行每一个运算的设备。
+    #
+    sess= tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    print(sess.run(c))
